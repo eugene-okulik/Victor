2f262041f4a8445cb63219ca91d505c0795caca8
Victor <victorkamylin@gmail.com>
2026-02-15 18:18:01 +0300

    added a folder homework/victor_kamylin/Homework_4/ with homework

---
+++ homework/victor_kamylin/Homework_4/task.py
@@ -0,0 +1,30 @@
+#
+
+my_dict = {
+    "tuple": (1, 3, None, True, "hello"),
+    "list": ["good", 5, False, 4.6, 5],
+    "dict": {
+        "BMW": "X6",
+        "Audi": "Q7",
+        "Mercedes": "C63",
+        "Opel": "Corsa",
+        "Toyota": "Yaris",
+    },
+    "set": {"BMW", "Audi", "Mercedes", "Opel", "Toyota"},
+}
+
+my_dict["list"].append("else_one_element")
+
+my_dict["list"].pop(1)
+
+my_dict["dict"]["Li"] = "L7"
+
+my_dict["dict"].pop("Audi")
+
+my_dict["set"].add("Honda")
+
+my_dict["set"].discard("Audi")
+
+print(my_dict["tuple"][-1])
+
+print(my_dict)
