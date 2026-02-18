1b88cfc7efe6ad3551a3b305c3c0012349ae248f
Victor <victorkamylin@gmail.com>
2026-02-18 09:21:52 +0300

    fixed formatting

--- homework/victor_kamylin/Homework_5/task_3.py
+++ homework/victor_kamylin/Homework_5/task_3.py
@@ -2,6 +2,9 @@ students = ["Ivanov", "Petrov", "Sidorov"]
 
 subjects = ["math", "biology", "geography"]
 
-res = f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}"
+res = (
+    f"Students {students[0]}, {students[1]}, {students[2]} "
+    f"study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}"
+)
 
 print(res)
