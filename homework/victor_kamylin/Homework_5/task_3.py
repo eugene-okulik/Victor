57a9faa7c3061c72b62c384b2e01a4f5c36eb71f
Victor <victorkamylin@gmail.com>
2026-02-18 08:55:14 +0300

    Homework_5 done

---
+++ homework/victor_kamylin/Homework_5/task_3.py
@@ -0,0 +1,7 @@
+students = ["Ivanov", "Petrov", "Sidorov"]
+
+subjects = ["math", "biology", "geography"]
+
+res = f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}"
+
+print(res)
