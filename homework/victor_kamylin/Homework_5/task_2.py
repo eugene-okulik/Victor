ee6c5b111c1722649b322c3f588ba69a7e118658
Victor <victorkamylin@gmail.com>
2026-02-18 10:18:37 +0300

    fixed formatting_2

--- homework/victor_kamylin/Homework_5/task_2.py
+++ homework/victor_kamylin/Homework_5/task_2.py
@@ -6,11 +6,11 @@ response_2 = "результат операции: 514"
 
 response_3 = "результат работы программы: 9"
 
-result_1 = int(response_1[response_1.index(":") + 2 :]) + 10
+result_1 = int(response_1[response_1.index(":") + 2:]) + 10
 
-result_2 = int(response_2[response_2.index(":") + 2 :]) + 10
+result_2 = int(response_2[response_2.index(":") + 2:]) + 10
 
-result_3 = int(response_3[response_3.index(":") + 2 :]) + 10
+result_3 = int(response_3[response_3.index(":") + 2:]) + 10
 
 print(result_1)
 
--- homework/victor_kamylin/Homework_5/task_3.py
+++ homework/victor_kamylin/Homework_5/task_3.py
@@ -1,7 +1,7 @@
-students = ["Ivanov", "Petrov", "Sidorov"]
+students = ', '.join(["Ivanov", "Petrov", "Sidorov"])
 
-subjects = ["math", "biology", "geography"]
+subjects = ', '.join(["math", "biology", "geography"])
 
-res = f"Students {students[0]}, {students[1]}, {students[2]} study these subjects: {subjects[0]}, {subjects[1]}, {subjects[2]}"
+res = f"Students {students} study these subjects: {subjects}"
 
 print(res)
