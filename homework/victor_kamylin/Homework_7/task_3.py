6029cdba49751efa8c2d91eeee3dc2a6bdebb39e
Victor <victorkamylin@gmail.com>
2026-02-22 10:33:20 +0300

    Homework_7 done

---
+++ homework/victor_kamylin/Homework_7/task_3.py
@@ -0,0 +1,10 @@
+def get_result(response):
+    return int(response.split(' ')[-1].strip()) +10
+
+print(get_result("результат операции: 42"))
+
+print(get_result("результат операции: 514"))
+
+print(get_result("результат работы программы: 9"))
+
+print(get_result("результат: 2"))
